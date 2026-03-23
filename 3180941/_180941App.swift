ef5afef9a0695bfb9c0665adee9224a3d6ac912e
Michael Cao <cxr20060821@gmail.com>
2026-03-23 13:37:09 +0800

    Initial Commit

---
+++ 3180941/_180941App.swift
@@ -0,0 +1,17 @@
+//
+//  _180941App.swift
+//  3180941
+//
+//  Created by student01 on 2026/3/23.
+//
+
+import SwiftUI
+
+@main
+struct _180941App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3180941Tests/_180941Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _180941Tests.swift
+//  3180941Tests
+//
+//  Created by student01 on 2026/3/23.
+//
+
+import Testing
+@testable import _180941
+
+struct _180941Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
